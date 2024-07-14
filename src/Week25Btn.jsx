79f775f663d1e86d20a84796bfec41764fd31bcb
Daria <scheglova.daria@gmail.com>
2024-07-14 18:09:09 +0300

    add week28-p1

--- src/Week25Btn.jsx
+++ src/Week25Btn.jsx
@@ -10,7 +10,34 @@ function Counter() {
     document.button = count;
   });
 
-  return <button onClick={handleClick}>{count}</button>;
+  // Практическое задание №1 - неделя 28 Формы
+  const [inputValue, setInputValue] = useState("");
+  const handleChange = (e) => {
+    setInputValue(e.target.value.toUpperCase());
+  };
+  const handleCheck = (e) => {
+    e.preventDefault();
+    document.getElementById("testText").textContent = inputValue;
+    document.getElementById("testText").style.color = "#0000ff";
+    setInputValue("");
+  };
+
+  return (
+    <React.Fragment>
+      <button onClick={handleClick}>{count}</button>;
+      {/* Практическое задание №1 - неделя 28 Формы */}
+      <div className="test_input">
+        <input
+          id="testInput"
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Submit" onClick={handleCheck} />
+        <p id="testText"></p>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Counter;
